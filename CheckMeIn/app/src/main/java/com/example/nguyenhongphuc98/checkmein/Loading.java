44e0e40905a74a90a76274e1bfe8350c45cdfd1c
Hong Phuc <nguyenhongphuc98@gmail.com>
2019-09-28 15:39:41 +0700

    create bottom navigate (use for all screen)

--- CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/Loading.java
+++ CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/Loading.java
@@ -8,6 +8,6 @@ public class Loading extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loading);
+        setContentView(R.layout.activity_home);
     }
 }

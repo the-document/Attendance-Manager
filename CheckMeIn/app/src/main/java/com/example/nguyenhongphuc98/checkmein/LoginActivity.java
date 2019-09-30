abce4ec5d90c2d19227b25673066db36741387a7
mirushi <luubieunghi@gmail.com>
2019-09-30 12:46:42 +0700

    Login, Register, Forgot pass
    Giao diện UI với chức năng chuyển qua lại giữa các Activity nêu trên

---
+++ CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/LoginActivity.java
@@ -0,0 +1,107 @@
+package com.example.nguyenhongphuc98.checkmein;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView txtLinkToRegister;
+    TextView txtLinkToForgotPass;
+
+    EditText edtUsername;
+    EditText edtPassword;
+
+    Button btnLogin;
+
+    boolean isRegisterSuccessful = false;
+    public static final String EXTRA_USERNAME = ".LOGIN.USERNAME";
+    public static final String EXTRA_PASSWORD = ".LOGIN.PASSWORD";
+    public static final int REGISTER_SUCCESS = 1;
+    public static final int REGISTER_NOT = 0;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Ánh xạ.
+        txtLinkToRegister = (TextView)findViewById(R.id.txtLinkToRegister);
+        txtLinkToForgotPass = (TextView)findViewById(R.id.txtLinkToForgotPass);
+        edtUsername = (EditText)findViewById(R.id.login_edtUsername);
+        edtPassword = (EditText)findViewById(R.id.login_edtPassword);
+        btnLogin = (Button)findViewById(R.id.login_btnLogin);
+
+        //Sư kiện.
+        txtLinkToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoToRegisterActivity(v);
+            }
+        });
+        txtLinkToForgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoToResetPasswordActivity(v);
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Login button clicked !", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+    }
+    protected void GoToRegisterActivity(View view)
+    {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        EditText userName = (EditText)findViewById(R.id.login_edtUsername);
+        EditText passWord = (EditText)findViewById(R.id.login_edtPassword);
+        String messageUserName = userName.getText().toString();
+        String messagePassword = passWord.getText().toString();
+        intent.putExtra(EXTRA_USERNAME, messageUserName);
+        intent.putExtra(EXTRA_PASSWORD, messagePassword);
+        startActivity(intent);
+    }
+    protected void GoToResetPasswordActivity(View view)
+    {
+        Intent intent = new Intent(this, ResetPasswordActivity.class);
+        startActivity(intent);
+    }
+    protected void SetupUsernamePassword(String userName, String passWord, boolean autoLogin)
+    {
+        edtUsername.setText(userName);
+        edtPassword.setText(passWord);
+        if (autoLogin)
+            btnLogin.performClick();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK)
+        {
+            boolean wantAutoLogin = false;
+            if (requestCode == REGISTER_SUCCESS)
+            {
+                wantAutoLogin = true;
+            }
+            else
+            {
+                wantAutoLogin = false;
+            }
+            SetupUsernamePassword(data.getStringExtra(EXTRA_USERNAME), data.getStringExtra((EXTRA_PASSWORD)), wantAutoLogin);
+        }
+        else
+        {
+            Toast toast = Toast.makeText(getApplicationContext(), "Error receive intent from register", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+}
---
+++ CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/RegisterActivity.java
@@ -0,0 +1,75 @@
+package com.example.nguyenhongphuc98.checkmein;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText edtUsername;
+    EditText edtPassword;
+    EditText edtPasswordConfirmation;
+    EditText edtEmail;
+    Button btnRegister;
+    TextView txtLinkToLogin;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //Ánh xạ.
+        edtUsername = (EditText)findViewById(R.id.register_edtUsername);
+        edtPassword = (EditText)findViewById(R.id.register_edtPassword);
+        edtPasswordConfirmation = (EditText)findViewById(R.id.register_edtConfirmPassword);
+        edtEmail = (EditText)findViewById(R.id.register_edtEmail);
+        btnRegister = (Button)findViewById(R.id.register_btnRegister);
+        txtLinkToLogin = (TextView)findViewById(R.id.register_linkToLogin);
+
+        //Lấy dữ liệu từ lúc chúng ta login.
+        Intent intent = getIntent();
+        String inputtedUsername = intent.getStringExtra(LoginActivity.EXTRA_USERNAME);
+        String inputtedPassword = intent.getStringExtra(LoginActivity.EXTRA_PASSWORD);
+
+        //Update lên luôn để người dùng đỡ phải nhập lại nhiều lần.
+        edtUsername.setText(inputtedUsername);
+        edtPassword.setText(inputtedPassword);
+
+        //Sự kiện.
+            txtLinkToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLoginActivity(v);
+            }
+        });
+
+            btnRegister.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    registerProcess(v);
+                }
+            });
+
+    }
+    protected void goToLoginActivity(View view)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        String userName = edtUsername.getText().toString();
+        String passWord = edtPassword.getText().toString();
+        intent.putExtra(LoginActivity.EXTRA_USERNAME, userName);
+        intent.putExtra(LoginActivity.EXTRA_PASSWORD, passWord);
+        startActivityForResult(intent, LoginActivity.REGISTER_NOT);
+    }
+    protected void registerProcess(View view)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        String userName = edtUsername.getText().toString();
+        String passWord = edtPassword.getText().toString();
+        intent.putExtra(LoginActivity.EXTRA_USERNAME, userName);
+        intent.putExtra(LoginActivity.EXTRA_PASSWORD, passWord);
+        startActivityForResult(intent, LoginActivity.REGISTER_SUCCESS);
+    }
+}

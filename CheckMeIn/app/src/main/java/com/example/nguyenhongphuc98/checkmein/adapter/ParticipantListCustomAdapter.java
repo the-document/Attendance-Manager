20d19aacc34db9d849e4069e8ef18c3ac40247cf
mirushi <luubieunghi@gmail.com>
2019-10-08 15:06:12 +0700

    Update full thống kê câu trả lời của user

--- CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/adapter/ParticipantListCustomAdapter.java
+++ CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/adapter/ParticipantListCustomAdapter.java
@@ -2,8 +2,14 @@ package com.example.nguyenhongphuc98.checkmein.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.example.nguyenhongphuc98.checkmein.R;
 import com.example.nguyenhongphuc98.checkmein.model.Participant;
 
 import java.util.ArrayList;
@@ -23,8 +29,10 @@ public class ParticipantListCustomAdapter extends ArrayAdapter<Participant> {
     }
 
     @Override
-    public int getCount()
-    {
+    public int getCount() {
         return participantArrayList.size();
     }
+
+
+
 }

a630bfd8fed16a0346e425547b129617ab3d6dd3
Nguyen Cao Luyen <16520713@gm.uit.edu.vn>
2019-10-11 17:50:35 +0700

    add datetime popup

---
+++ CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/DatetimeDialogFragment.java
@@ -0,0 +1,27 @@
+package com.example.nguyenhongphuc98.checkmein;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+public class DatetimeDialogFragment extends DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.datetime_popup, container, false);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
+}
--- CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/SendEmailFragment.java
+++ CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/SendEmailFragment.java
@@ -1,5 +1,9 @@
 package com.example.nguyenhongphuc98.checkmein;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,10 +13,25 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class SendEmailFragment extends Fragment {
+    View btnClock;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_send_email, container, false);
+        btnClock = (View) view.findViewById(R.id.btnClock);
+        btnClock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*final Dialog dtDialogue = new Dialog(getActivity(), android.R.style.Theme_Black_NoTitleBar);
+                dtDialogue.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(100, 0, 0, 0)));
+                dtDialogue.setContentView(R.layout.datetime_popup);
+                dtDialogue.setCancelable(true);
+                dtDialogue.show();*/
+                DatetimeDialogFragment dtDialog = new DatetimeDialogFragment();
+                dtDialog.show(getFragmentManager(), "dialog");
+            }
+        });
         return view;
     }
 }

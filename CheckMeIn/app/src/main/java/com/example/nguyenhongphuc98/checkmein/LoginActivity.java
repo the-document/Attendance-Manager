e2f615cacccf86f1e4faf1e284948db74bc15009
Hong Phuc <nguyenhongphuc98@gmail.com>
2019-10-21 20:14:28 +0700

    change allow large image size

--- CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/LoginActivity.java
+++ CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/LoginActivity.java
@@ -68,7 +68,6 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-
     private void ProcessLoginData() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);

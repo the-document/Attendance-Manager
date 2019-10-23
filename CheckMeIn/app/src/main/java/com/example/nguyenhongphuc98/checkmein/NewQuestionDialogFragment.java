8902a7094fc6c7e0241010c46b72a42b552e0d7d
mirushi <luubieunghi@gmail.com>
2019-10-23 08:35:54 +0700

    Update Search UI

--- CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/NewQuestionDialogFragment.java
+++ CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/NewQuestionDialogFragment.java
@@ -4,17 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 public class NewQuestionDialogFragment extends DialogFragment {
 
     private EditText mEdtQuestion;
     private EditText mEdtAnswer1;
-    private Button mButtonAddQuestion;
+    private ArrayList<EditText> additionalAnswers = new ArrayList<EditText>();
+
+    private Button mButtonAddAnswer;
     private Button mButtonFinish;
 
     public NewQuestionDialogFragment()
@@ -37,17 +42,31 @@ public class NewQuestionDialogFragment extends DialogFragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //Ánh xạ.
         AssignAllControl(view);
+
+        //Thêm listener bắt sự kiện khi người dùng muốn thêm câu hỏi.
+        mButtonAddAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText newAnswer = new EditText(getContext());
+
+            }
+        });
     }
 
     private void AssignAllControl(View view)
     {
         mEdtQuestion = view.findViewById(R.id.fragment_add_question_and_answers_edt_question);
         mEdtAnswer1 = view.findViewById(R.id.fragment_add_question_and_answers_answer_1);
-        mButtonAddQuestion = view.findViewById(R.id.fragment_add_question_and_answers_btn_add_question);
+        mButtonAddAnswer = view.findViewById(R.id.fragment_add_question_and_answers_btn_add_answer);
         mButtonFinish = view.findViewById(R.id.fragment_add_question_and_answers_btn_accept);
     }
 }

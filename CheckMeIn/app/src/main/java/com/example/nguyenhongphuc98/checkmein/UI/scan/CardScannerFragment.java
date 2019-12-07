8d1e249705f5769002a84e224a6b737cf53bc350
nghi dep trai <luubieunghi@gmail.com>
2019-12-07 16:28:19 +0700

    Card scanning animation

--- CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/UI/scan/CardScannerFragment.java
+++ CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/UI/scan/CardScannerFragment.java
@@ -1,28 +1,23 @@
 package com.example.nguyenhongphuc98.checkmein.UI.scan;
 
-import android.Manifest;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
-import android.os.Build;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.example.nguyenhongphuc98.checkmein.R;
-import com.example.nguyenhongphuc98.checkmein.UI.scan.CameraPreview;
-import com.tbruyelle.rxpermissions2.RxPermissions;
 
 public class CardScannerFragment extends Fragment {
 
@@ -30,6 +25,9 @@ public class CardScannerFragment extends Fragment {
     private Camera mCamera;
     private CameraPreview mPreview;
     FrameLayout previewLayout;
+    ConstraintLayout layout;
+    ImageView imgViewCardScanningBarLeft;
+    ImageView imgViewCardScanningBarRight;
 
     @Nullable
     @Override
@@ -38,9 +36,22 @@ public class CardScannerFragment extends Fragment {
         mCamera = getCameraInstance(mPreview);
         mPreview = new CameraPreview(view.getContext(), mCamera);
 
+        //Ánh xạ.
+        layout = (ConstraintLayout)view.findViewById(R.id.fragment_card_scanner_layout);
+        imgViewCardScanningBarLeft = (ImageView)view.findViewById(R.id.fragment_card_scanner_scanning_bar);
+        imgViewCardScanningBarRight = (ImageView)view.findViewById(R.id.fragment_card_scanner_scanning_bar_2);
+
         //Cài đặt preview camera.
         previewLayout = view.findViewById(R.id.fv_camera_preview);
         previewLayout.addView(mPreview);
+
+        //Animation cho thanh chạy.
+        final Animation scanningAnimationBottomToTop = AnimationUtils.loadAnimation(getContext(), R.anim.custom_scanning_moving_bar_bottom_to_top);
+        final Animation scanningAnimationTopToBottom = AnimationUtils.loadAnimation(getContext(), R.anim.custom_scanning_moving_bar_top_to_bottom);
+
+        //Chạy animation.
+        imgViewCardScanningBarLeft.startAnimation(scanningAnimationBottomToTop);
+        imgViewCardScanningBarRight.startAnimation(scanningAnimationTopToBottom);
 
         return view;
     }

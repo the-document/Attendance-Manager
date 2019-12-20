11ecca93db464c605e82dc2376e3a870561753d1
nguyenhongphuc98 <nguyenhongphuc98@gmail.com>
2019-12-20 12:20:31 +0700

    join to event with code (check date)

--- CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/UI/home/HomePresenter.java
+++ CheckMeIn/app/src/main/java/com/example/nguyenhongphuc98/checkmein/UI/home/HomePresenter.java
@@ -1,13 +1,15 @@
 package com.example.nguyenhongphuc98.checkmein.UI.home;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.nguyenhongphuc98.checkmein.Data.DataCenter;
+import com.example.nguyenhongphuc98.checkmein.Data.db.model.Event;
 import com.example.nguyenhongphuc98.checkmein.Data.network.DataManager;
 
 import java.util.List;
 
-public class HomePresenter implements IHome{
+public class HomePresenter implements IHome, IEventCallBack{
     HomeFragment view;
 
     public HomePresenter(HomeFragment v){
@@ -20,7 +22,31 @@ public class HomePresenter implements IHome{
     }
 
     @Override
+    public void OnRequestJoinEvent() {
+        DataManager.Instance().setEventCallBacks(this);
+        DataManager.Instance().LoadEventByCode(view.etActivityCode.getText().toString());
+    }
+
+    @Override
     public void OnLoadOrganSuccess() {
 
+    }
+
+    @Override
+    public void OnJoinEventSucess(Event event) {
+
+    }
+
+    @Override
+    public void OnJoinEventFail(Event event) {
+
+    }
+
+    @Override
+    public void OnLoadEventComplete(Event event) {
+        if(event!=null)
+            view.OnJoinEventSucess(event);
+        else
+            view.OnJoinEventFail(event);
     }
 }
